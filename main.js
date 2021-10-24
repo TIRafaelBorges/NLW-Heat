c2e4e0dab90b8bb945f8ecfedb3e14b6471f7b9d
RafaelBorges.TI <92737961+TIRafaelBorges@users.noreply.github.com>
2021-10-23 23:34:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const LinksSocialMedia = {
   github: 'TIRafaelBorges',
-  //youtube: ,
-  //facebook: ,
-  instagram: 'ti.rafaelborges'
-  //twitter: 
+  youtube: null,
+  facebook: null,
+  instagram: 'ti.rafaelborges',
+  twitter: null 
 }
 
 function changeSocialMediaLinks() {
